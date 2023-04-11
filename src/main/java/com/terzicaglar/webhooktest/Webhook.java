83e7e34dc6ac5985e422b9f75b1da0982ce807c9
Caglar Terzi <cterzi@cyangate.com>
2023-04-11 18:45:39 +0300

    commit 3

--- src/main/java/com/terzicaglar/webhooktest/Webhook.java
+++ src/main/java/com/terzicaglar/webhooktest/Webhook.java
@@ -2,6 +2,7 @@ package com.terzicaglar.webhooktest;
 
 public class Webhook {
     public static void main(String[] args) {
+        //commit 3
         System.out.println("webhook test");
         System.out.println(divide(19, 2));
 
