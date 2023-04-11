fe1a33420ddc9cbcdbc41237c3ead0472b961b30
Caglar Terzi <cterzi@cyangate.com>
2023-04-11 18:45:27 +0300

    commit 2

--- src/main/java/com/terzicaglar/webhooktest/Webhook.java
+++ src/main/java/com/terzicaglar/webhooktest/Webhook.java
@@ -4,7 +4,7 @@ public class Webhook {
     public static void main(String[] args) {
         System.out.println("webhook test");
         System.out.println(divide(19, 2));
-        //commit 1
+
     }
 
     public static double divide(int a, int b) {
