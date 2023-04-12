574d899bd10bf1f8b71eed8d3bac487e209fa730
Caglar Terzi <cterzi@cyangate.com>
2023-04-12 12:10:57 +0300

    implement matrix generation

--- src/main/java/com/terzicaglar/webhooktest/Webhook.java
+++ src/main/java/com/terzicaglar/webhooktest/Webhook.java
@@ -1,32 +1,32 @@
 package com.terzicaglar.webhooktest;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Webhook {
     public static void main(String[] args) {
         //commit 3
         System.out.println("webhook test");
-        System.out.println(divide(19, 2));
 
     }
 
-    public static double divide(int a, int b) {
-        //divided two integers and return double value
-        return (double) a / b;
-    }
+    /**
+     * This method will generate a matrix of size n x n, filled with random
+     * numbers between 0 and 100
+     *
+     * @param n
+     *            The size of the matrix
+     * @return an array of size n x n
+     */
+    public static int[][] generateMatrix(int n) {
+        Random r = new Random();
+        int[][] matrix = new int[n][n];
 
-    public static double average(int[] ar) {
-        return Arrays.stream(ar).average().getAsDouble();
-    }
-
-    public static void move(Direction direction) {
-
-    }
-
-    private enum Direction {
-        SOUTH,
-        WEST,
-        EAST,
-        NORTH
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = r.nextInt(100);
+            }
+        }
+        return matrix;
     }
 }
