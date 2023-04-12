edacabfd4b6ea49fd522336cb405a84847f33d6e
Caglar Terzi <cterzi@cyangate.com>
2023-04-12 12:20:15 +0300

    implement classic matrix multiplication

--- src/main/java/com/terzicaglar/webhooktest/Webhook.java
+++ src/main/java/com/terzicaglar/webhooktest/Webhook.java
@@ -56,4 +56,23 @@ public class Webhook {
             a++;
         }
     }
+
+    public static int[][] classicMM(int[][] A, int[][] B, int n) {
+        int[][] C = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
+    }
 }
